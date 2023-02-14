2a3921bfd77090ad2888829568c181f3d0f55820
Daniela Leguari <danielaleguari@outlook.com>
2023-02-14 19:01:05 -0300

    componente pesquisa

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import './App.css';
 import Header from './components/Header';
 import styled from 'styled-components';
+import Pesquisa from './components/Pesquisa';
 
 const AppContainer = styled.main`
       width: 100vw;
       height: 100vh;
-      background: rgb(240,205,220);
-      background: radial-gradient(circle, rgba(240,205,220,0.8465511204481793) 0%, rgba(241,240,229,1) 100%);
+      background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
 `
 
 function App() {
   return (
     <AppContainer>
       <Header />
+      <Pesquisa />
     </AppContainer>
   );
 }
---
+++ src/components/Input/index.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+    order: 1px solid #F6f6f6;
+    background: transparent;
+    border: 1px solid #F6f6f6;
+    padding: 20px 140px;
+    border-radius: 50px;
+    width: 200px;
+    color: #f6f6f6;
+    font-size: 1rem;
+    margin-bottom: 10px;
+
+    &::placeholder {
+        color: #F6f6f6;
+        font-size: 16px;
+    }   
+`
+
+
+export default Input;
